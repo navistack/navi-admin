f60fbdc52b6fe476800350a3b03886a52479e896
ieu <5358482+ieu@users.noreply.github.com>
2022-07-20 00:39:10 +0800

    enhancement: disable simple captcha endpoint on SimpleCaptchaService missing

--- src/main/java/org/navistack/admin/modules/system/web/rest/SysSimpleCaptchaController.java
+++ src/main/java/org/navistack/admin/modules/system/web/rest/SysSimpleCaptchaController.java
@@ -2,11 +2,13 @@ package org.navistack.admin.modules.system.web.rest;
 
 import org.navistack.framework.captcha.simplecaptcha.AbstractSimpleCaptchaController;
 import org.navistack.framework.captcha.simplecaptcha.SimpleCaptchaService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sys/simple-captcha")
+@ConditionalOnBean(SimpleCaptchaService.class)
 public class SysSimpleCaptchaController extends AbstractSimpleCaptchaController {
     public SysSimpleCaptchaController(SimpleCaptchaService simpleCaptchaService) {
         super(simpleCaptchaService);
