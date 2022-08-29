082d699cbe6f039ad61f6de5e3b1a03f633861b9
ieu <5358482+ieu@users.noreply.github.com>
2022-08-29 22:41:05 +0800

    feat: api to get region at any level recursively or non-recursively

--- src/main/java/org/navistack/admin/modules/common/dao/RegionDao.java
+++ src/main/java/org/navistack/admin/modules/common/dao/RegionDao.java
@@ -1,9 +1,15 @@
 package org.navistack.admin.modules.common.dao;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.navistack.admin.modules.common.entity.Region;
 import org.navistack.framework.mybatisplusplus.CrudMapper;
 
+import java.util.List;
+
 @Mapper
 public interface RegionDao extends CrudMapper<Region> {
+    List<Region> selectListRecursively(@Param(Constants.WRAPPER) Wrapper<Region> queryWrapper);
 }
--- src/main/java/org/navistack/admin/modules/system/web/rest/SysRegionController.java
+++ src/main/java/org/navistack/admin/modules/system/web/rest/SysRegionController.java
@@ -1,5 +1,6 @@
 package org.navistack.admin.modules.system.web.rest;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -7,12 +8,12 @@ import org.navistack.admin.modules.common.dao.RegionDao;
 import org.navistack.admin.modules.common.entity.Region;
 import org.navistack.admin.modules.system.web.rest.vm.RegionVm;
 import org.navistack.framework.data.TreeUtils;
+import org.navistack.framework.utils.Strings;
 import org.navistack.framework.web.rest.RestResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -31,6 +32,32 @@ public class SysRegionController {
     public RestResult.Ok<Collection<RegionVm>> get() {
         List<Region> regions = regionDao.selectList(Wrappers.emptyWrapper());
         Collection<RegionVm> vms = TreeUtils.treeize(regions, RegionVm::of);
+        return RestResult.ok(vms);
+    }
+
+    @GetMapping("/{region}")
+    @Operation(summary = "Get regions and their sub-regions recursively")
+    @Tag(name = "System")
+    public RestResult.Ok<Collection<RegionVm>> get(
+            @PathVariable("region") String regionCode,
+            @RequestParam(defaultValue = "true") boolean recursive
+    ) {
+        List<Region> regions = null;
+        if (recursive) {
+            Wrapper<Region> wrapper = Wrappers.<Region>lambdaQuery()
+                    .eq(Strings.hasText(regionCode), Region::getCode, regionCode);
+            regions = regionDao.selectListRecursively(wrapper);
+        } else {
+            Wrapper<Region> wrapper = Wrappers.<Region>lambdaQuery()
+                    .eq(Strings.hasText(regionCode), Region::getParentCode, regionCode);
+            regions = regionDao.selectList(wrapper);
+        }
+
+        if (regions.isEmpty()) {
+            return RestResult.ok(Collections.emptyList());
+        }
+
+        Collection<RegionVm> vms = TreeUtils.treeize(regions, regionCode, RegionVm::of);
         return RestResult.ok(vms);
     }
 }
