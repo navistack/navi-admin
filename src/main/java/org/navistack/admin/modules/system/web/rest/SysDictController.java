aaab5c2afd76ccec94dd7f892b03c183f63d8f98
ieu <5358482+ieu@users.noreply.github.com>
2022-08-29 22:41:04 +0800

    feat: api to get single directory

--- src/main/java/org/navistack/admin/modules/system/web/rest/SysDictController.java
+++ src/main/java/org/navistack/admin/modules/system/web/rest/SysDictController.java
@@ -1,19 +1,25 @@
 package org.navistack.admin.modules.system.web.rest;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.navistack.admin.modules.common.dao.DictDao;
 import org.navistack.admin.modules.common.dao.DictItemDao;
+import org.navistack.admin.modules.common.entity.Dict;
 import org.navistack.admin.modules.common.entity.DictItem;
 import org.navistack.admin.modules.system.web.rest.vm.DictItemVm;
 import org.navistack.admin.modules.system.web.rest.vm.DictVm;
+import org.navistack.framework.utils.Strings;
 import org.navistack.framework.web.rest.RestResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -58,5 +64,34 @@ public class SysDictController {
         }
 
         return RestResult.ok(dictVms);
+    }
+
+    @GetMapping("/{dict:[A-Za-z0-9$_]{1,48}}")
+    @Operation(summary = "Get directory and its items")
+    @Tag(name = "System")
+    public RestResult.Ok<DictVm> get(@PathVariable("dict") String dictCode) {
+        Wrapper<Dict> dictWrapper = Wrappers.<Dict>lambdaQuery()
+                .eq(Strings.hasText(dictCode), Dict::getCode, dictCode);
+
+        Dict dict = dictDao.selectOne(dictWrapper);
+        if (dict == null) {
+            return RestResult.ok(null);
+        }
+
+        DictVm dictVm = DictVm.of(dict.getCode(), dict.getName());
+
+        Wrapper<DictItem> itemWrapper = Wrappers.<DictItem>lambdaQuery()
+                .eq(Strings.hasText(dictCode), DictItem::getDictCode, dictCode);
+        List<DictItem> items = dictItemDao.selectList(itemWrapper);
+        if (items.isEmpty()) {
+            return RestResult.ok(dictVm);
+        }
+
+        Collection<DictItemVm> dictVms = items.stream()
+                .map(item -> DictItemVm.of(item.getName(), item.getItKey(), item.getItValue()))
+                .collect(Collectors.toList());
+        dictVm.setItems(dictVms);
+
+        return RestResult.ok(dictVm);
     }
 }
