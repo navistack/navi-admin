02e1bc4ab0039a89caf97e334aefc3f771531989
ieu <5358482+ieu@users.noreply.github.com>
2023-02-14 23:46:49 +0800

    test: create test for RoleServiceImpl

--- src/main/java/org/navistack/admin/modules/mgmt/service/impl/RoleServiceImpl.java
+++ src/main/java/org/navistack/admin/modules/mgmt/service/impl/RoleServiceImpl.java
@@ -3,20 +3,25 @@ package org.navistack.admin.modules.mgmt.service.impl;
 import org.navistack.admin.modules.common.dao.PrivilegeDao;
 import org.navistack.admin.modules.common.dao.RoleDao;
 import org.navistack.admin.modules.common.dao.RolePrivilegeDao;
+import org.navistack.admin.modules.common.dao.UserRoleDao;
 import org.navistack.admin.modules.common.entity.Role;
 import org.navistack.admin.modules.common.entity.RolePrivilege;
 import org.navistack.admin.modules.common.query.PrivilegeQuery;
 import org.navistack.admin.modules.common.query.RolePrivilegeQuery;
 import org.navistack.admin.modules.common.query.RoleQuery;
+import org.navistack.admin.modules.common.query.UserRoleQuery;
 import org.navistack.admin.modules.mgmt.service.RoleService;
 import org.navistack.admin.modules.mgmt.service.convert.RoleConverter;
 import org.navistack.admin.modules.mgmt.service.dto.RoleDto;
 import org.navistack.admin.modules.mgmt.service.vm.RoleDetailVm;
 import org.navistack.admin.support.mybatis.AuditingEntitySupport;
-import org.navistack.framework.core.error.EntityDuplicationException;
+import org.navistack.framework.core.error.ConstraintViolationException;
+import org.navistack.framework.core.error.DomainValidationException;
+import org.navistack.framework.core.error.NoSuchEntityException;
 import org.navistack.framework.data.Page;
 import org.navistack.framework.data.PageImpl;
 import org.navistack.framework.data.Pageable;
+import org.navistack.framework.utils.Asserts;
 import org.navistack.framework.utils.ModelMappers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,14 +35,17 @@ public class RoleServiceImpl implements RoleService {
     private final RoleDao dao;
     private final PrivilegeDao privilegeDao;
     private final RolePrivilegeDao rolePrivilegeDao;
+    private final UserRoleDao userRoleDao;
 
     public RoleServiceImpl(
             RoleDao roleDao,
             PrivilegeDao privilegeDao,
-            RolePrivilegeDao rolePrivilegeDao) {
+            RolePrivilegeDao rolePrivilegeDao,
+            UserRoleDao userRoleDao) {
         this.dao = roleDao;
         this.privilegeDao = privilegeDao;
         this.rolePrivilegeDao = rolePrivilegeDao;
+        this.userRoleDao = userRoleDao;
     }
 
     @Override
@@ -69,7 +77,7 @@ public class RoleServiceImpl implements RoleService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void create(RoleDto dto) {
-        ensureUnique(dto);
+        Asserts.state(dto.getCode(), dto.getId(), this::validateAvailabilityOfCode, () -> new DomainValidationException("Role code has been taken already"));
 
         Role entity = RoleConverter.INSTANCE.dtoToEntity(dto);
         AuditingEntitySupport.insertAuditingProperties(entity);
@@ -80,7 +88,8 @@ public class RoleServiceImpl implements RoleService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void modify(RoleDto dto) {
-        ensureUnique(dto);
+        Asserts.state(dto.getId(), this::validateExistenceById, () -> new NoSuchEntityException("Role does not exist"));
+        Asserts.state(dto.getCode(), dto.getId(), this::validateAvailabilityOfCode, () -> new DomainValidationException("Role code has been taken already"));
 
         Role entity = RoleConverter.INSTANCE.dtoToEntity(dto);
         AuditingEntitySupport.updateAuditingProperties(entity);
@@ -89,24 +98,17 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void remove(Long id) {
+        Asserts.state(id, this::validateExistenceById, () -> new NoSuchEntityException("Role does not exist"));
+        Asserts.state(id, this::validateAbsenceOfUser, () -> new ConstraintViolationException("Role can not be removed due to user(s) attached"));
+
         dao.deleteById(id);
-    }
 
-    protected void ensureUnique(RoleDto dto) {
-        RoleQuery queryDto = new RoleQuery();
-        queryDto.setCode(dto.getCode());
-        Role existedOne = dao.selectOne(queryDto);
-
-        if (existedOne == null) {
-            return;
-        }
-
-        if (!existedOne.getId().equals(dto.getId())) {
-            return;
-        }
-
-        throw new EntityDuplicationException("Role already exists");
+        RolePrivilegeQuery rpq = RolePrivilegeQuery.builder()
+                .roleId(id)
+                .build();
+        rolePrivilegeDao.delete(rpq);
     }
 
     protected void replacePrivilegesOf(Long roleId, List<Long> privilegeIds) {
@@ -129,4 +131,40 @@ public class RoleServiceImpl implements RoleService {
             }
         }
     }
+
+    // region Validation methods
+
+    protected boolean validateExistenceById(Long id) {
+        return id != null && dao.selectOneById(id) != null;
+    }
+
+    protected boolean validateAvailabilityOfCode(String code, Long modifiedId) {
+        RoleQuery query = RoleQuery.builder()
+                .code(code)
+                .build();
+        Role existingOne = dao.selectOne(query);
+
+        if (existingOne == null) {
+            return true;
+        }
+
+        if (!existingOne.getCode().equals(code)) {
+            return true;
+        }
+
+        if (existingOne.getId().equals(modifiedId)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected boolean validateAbsenceOfUser(Long roleId) {
+        UserRoleQuery query = UserRoleQuery.builder()
+                .roleId(roleId)
+                .build();
+        return userRoleDao.count(query) <= 0;
+    }
+
+    // endregion
 }
---
+++ src/test/java/org/navistack/admin/modules/mgmt/service/impl/RoleServiceImplTest.java
@@ -0,0 +1,158 @@
+package org.navistack.admin.modules.mgmt.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.navistack.admin.modules.common.dao.RoleDao;
+import org.navistack.admin.modules.common.dao.RolePrivilegeDao;
+import org.navistack.admin.modules.common.dao.UserRoleDao;
+import org.navistack.admin.modules.common.entity.Role;
+import org.navistack.admin.modules.common.query.RolePrivilegeQuery;
+import org.navistack.admin.modules.common.query.RoleQuery;
+import org.navistack.admin.modules.common.query.UserRoleQuery;
+import org.navistack.admin.modules.mgmt.service.dto.RoleDto;
+import org.navistack.framework.core.error.ConstraintViolationException;
+import org.navistack.framework.core.error.DomainValidationException;
+import org.navistack.framework.core.error.NoSuchEntityException;
+import org.navistack.framework.utils.GenericBuilder;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RoleServiceImplTest {
+    @InjectMocks
+    private RoleServiceImpl service;
+
+    @Mock
+    private RoleDao dao;
+
+    @Mock
+    private RolePrivilegeDao rolePrivilegeDao;
+
+    @Mock
+    private UserRoleDao userRoleDao;
+
+
+    @Test
+    void create_shouldCreateSuccessfully() {
+        RoleDto dto = new RoleDto();
+        dto.setCode("DEPT_MGR");
+        dto.setName("Department Manager");
+        service.create(dto);
+        verify(dao, times(1)).insert(any());
+    }
+
+    @Test
+    void create_shouldThrowDomainValidationExceptionWhenCodeDuplicated() {
+        when(dao.selectOne(
+                RoleQuery.builder()
+                        .code("DEPT_MGR")
+                        .build()
+        )).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 1L)
+                        .set(Role::setCode, "DEPT_MGR")
+                        .set(Role::setName, "Department Manager")
+                        .build()
+        );
+        assertThatThrownBy(() -> {
+            RoleDto dto = new RoleDto();
+            dto.setCode("DEPT_MGR");
+            dto.setName("Department Manager");
+            service.create(dto);
+        }).isInstanceOf(DomainValidationException.class);
+    }
+
+    @Test
+    void modify_shouldModifySuccessfully() {
+        when(dao.selectOneById(1L)).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 1L)
+                        .set(Role::setCode, "DEPT_MGR")
+                        .set(Role::setName, "Department Manager")
+                        .build()
+        );
+        RoleDto dto = new RoleDto();
+        dto.setId(1L);
+        dto.setCode("DEPT_MGR");
+        dto.setName("Manager of Department");
+        service.modify(dto);
+        verify(dao, times(1)).updateById(any());
+    }
+
+    @Test
+    void modify_shouldThrowDomainValidationExceptionWhenCodeDuplicated() {
+        when(dao.selectOne(
+                RoleQuery.builder()
+                        .code("GRL_MGR")
+                        .build()
+        )).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 1L)
+                        .set(Role::setCode, "GRL_MGR")
+                        .set(Role::setName, "General Manager")
+                        .build()
+        );
+        when(dao.selectOneById(2L)).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 2L)
+                        .set(Role::setCode, "DEPT_MGR")
+                        .set(Role::setName, "Department Manager")
+                        .build()
+        );
+        assertThatThrownBy(() -> {
+            RoleDto dto = new RoleDto();
+            dto.setId(2L);
+            dto.setCode("GRL_MGR");
+            dto.setName("General Manager");
+            service.modify(dto);
+        }).isInstanceOf(DomainValidationException.class);
+    }
+
+    @Test
+    void remove_shouldRemoveSuccessfully() {
+        when(dao.selectOneById(1L)).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 1L)
+                        .set(Role::setCode, "DEPT_MGR")
+                        .set(Role::setName, "Department Manager")
+                        .build()
+        );
+        service.remove(1L);
+        verify(dao, times(1)).deleteById(any());
+        verify(rolePrivilegeDao, times(1)).delete(any(RolePrivilegeQuery.class));
+    }
+
+    @Test
+    void remove_shouldThrowConstraintViolationExceptionWhenUserExists() {
+        when(dao.selectOneById(1L)).thenReturn(
+                GenericBuilder.of(Role::new)
+                        .set(Role::setId, 1L)
+                        .set(Role::setCode, "DEPT_MGR")
+                        .set(Role::setName, "Department Manager")
+                        .build()
+        );
+        when(userRoleDao.count(
+                UserRoleQuery.builder()
+                        .roleId(1L)
+                        .build()
+        )).thenReturn(10L);
+        assertThatThrownBy(() -> {
+            service.remove(1L);
+        }).isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    void remove_shouldThrowNoSuchEntityExceptionWhenIdIsWrong() {
+        assertThatThrownBy(() -> {
+            service.remove(null);
+        }).isInstanceOf(NoSuchEntityException.class);
+        assertThatThrownBy(() -> {
+            service.remove(100L);
+        }).isInstanceOf(NoSuchEntityException.class);
+    }
+}
