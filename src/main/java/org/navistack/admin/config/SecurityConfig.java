f0873a3c91035e7aba9dddaf43d8d4d0f3d24bef
ieu <5358482+ieu@users.noreply.github.com>
2023-07-04 22:00:05 +0800

    refactor: adapted to the latest snapshot of navi-boot

--- src/main/java/org/navistack/admin/config/SecurityConfig.java
+++ src/main/java/org/navistack/admin/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package org.navistack.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,36 +25,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
         return http
-                .headers()
-                    .xssProtection()
-                    .and()
-                    .contentSecurityPolicy("script-src 'self'")
-                    .and()
-                .and()
-                .authorizeHttpRequests()
-                    .requestMatchers("/login")
-                        .anonymous()
-                    .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
-                        .anonymous()
-                    .requestMatchers("/sys/simple-captcha/**")
-                        .anonymous()
-                    .anyRequest()
-                        .authenticated()
-                .and()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .csrf()
-                    .disable()
-                .httpBasic()
-                    .disable()
-                .formLogin()
-                    .disable()
-                .logout()
-                    .disable()
+                .headers(headers -> headers
+                        .xssProtection(Customizer.withDefaults())
+                        .contentSecurityPolicy(policy -> policy
+                                .policyDirectives("script-src 'self'")
+                        )
+                )
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/login").anonymous()
+                        .requestMatchers("/swagger-ui.html").anonymous()
+                        .requestMatchers("/swagger-ui/**").anonymous()
+                        .requestMatchers("/v3/api-docs/**").anonymous()
+                        .requestMatchers("/sys/simple-captcha/**").anonymous()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(CsrfConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .logout(LogoutConfigurer::disable)
                 .build();
-        // @formatter:on
     }
 }
