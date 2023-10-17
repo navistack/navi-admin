41b0f650812ff6912d78eb85c4e6466c66dd43d1
ieu <5358482+ieu@users.noreply.github.com>
2023-10-18 00:50:17 +0800

    refactor: adapted to the latest snapshot of navi-boot

--- src/main/java/org/navistack/admin/modules/system/web/rest/SysSimpleCaptchaController.java
+++ src/main/java/org/navistack/admin/modules/system/web/rest/SysSimpleCaptchaController.java
@@ -1,16 +1,57 @@
 package org.navistack.admin.modules.system.web.rest;
 
-import org.navistack.framework.captcha.simplecaptcha.AbstractSimpleCaptchaController;
 import org.navistack.framework.captcha.simplecaptcha.SimpleCaptchaService;
+import org.navistack.framework.captcha.simplecaptcha.UserAttemptResult;
+import org.navistack.framework.web.rest.RestResult;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 @RestController
 @RequestMapping("/sys/simple-captcha")
 @ConditionalOnBean(SimpleCaptchaService.class)
-public class SysSimpleCaptchaController extends AbstractSimpleCaptchaController {
+public class SysSimpleCaptchaController {
+
+    private final SimpleCaptchaService simpleCaptchaService;
+
     public SysSimpleCaptchaController(SimpleCaptchaService simpleCaptchaService) {
-        super(simpleCaptchaService);
+        this.simpleCaptchaService = simpleCaptchaService;
+    }
+
+    @PostMapping("/challenge")
+    public RestResult<String, ?> challenge() {
+        return RestResult.ok(simpleCaptchaService.challenge());
+    }
+
+    @PostMapping("/answer")
+    public RestResult<UserAttemptResult, ?> answer(String challengeId, String answer) {
+        return RestResult.ok(simpleCaptchaService.answer(challengeId, answer));
+    }
+
+    @GetMapping("/image")
+    public ResponseEntity<Resource> image(String challenge) throws Exception {
+        RenderedImage image = simpleCaptchaService.draw(challenge);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.noCache())
+                .contentType(MediaType.IMAGE_PNG)
+                .body(new InputStreamResource(byteArrayInputStream));
     }
 }
