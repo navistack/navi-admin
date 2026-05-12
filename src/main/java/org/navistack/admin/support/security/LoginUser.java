78cd219b9307fc5a3236ce8163fb94152418ba3e
ieu <5358482+ieu@users.noreply.github.com>
2026-05-12 23:30:13 +0800

    refactor: remove unnecessary methods from LoginUser class

--- src/main/java/org/navistack/admin/support/security/LoginUser.java
+++ src/main/java/org/navistack/admin/support/security/LoginUser.java
@@ -12,29 +12,4 @@ public class LoginUser implements UserDetails {
     private String username;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
